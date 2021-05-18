380ace40747c3067fdcf87623a050984cdc439cd
Bayan <bayany1379@gmail.com>
2021-05-18 11:25:22 +0430

    cleaning master branch

--- Templates/SimpleUserBot/bot_config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config(object):
-    APP_ID = int(os.environ.get('APP_ID',1234567))
-    API_HASH = os.environ.get('API_HASH',"3231uyc35find9ffe1yours12fe5q226")
-    PORT= int(os.environ.get('PORT',5000))
-    DATABASE_URL=os.environ.get('DATABASE_URL')
--- Templates/SimpleUserBot/database/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from bot_config import Config
-
-engine = create_engine(Config.DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
